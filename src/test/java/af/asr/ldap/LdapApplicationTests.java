1d9cacf33470e6ec52e1fe5a0acc89fa514719bc
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-23 09:26:03 +0430

    #1

---
+++ src/test/java/af/asr/ldap/LdapApplicationTests.java
@@ -0,0 +1,13 @@
+package af.asr.ldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
